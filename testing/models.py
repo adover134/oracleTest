e3009023aca5edc7f335d7b81b2fcc0e7936e5dc
adover134 <adfsfsf@naver.com>
2022-04-26 14:08:34 +0900

    장고 RESTframework 이용 예제

--- testing/models.py
+++ testing/models.py
@@ -7,3 +7,12 @@ class User(models.Model):
 
     class Meta:
         db_table = "user"
+
+
+class TestClass(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    test_id = models.IntegerField(primary_key=True)
+    u_name = models.TextField()
+
+    class Meta:
+        db_table = "testing"
